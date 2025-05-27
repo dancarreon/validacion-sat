c78236db54966bea10479de77cae57a91bbaa4a2
Daniel Carreon <daniel.carreong@gmail.com>
2025-05-27 15:26:42 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Header} from "./components/Header.tsx";
+import './App.css';
+
+const App = () => {
+    return (
+        <div className='w-full h-screen'>
+            <Header/>
+        </div>
+    )
+}
+export default App
